e107126fef626b0dfb2d4a05fd64c69773ceb2a4
WarHatch <staskeviciuskarolis@gmail.com>
2018-03-22 19:15:11 +0200

    Partially solved 1

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add first, second
+    first + second
+end
+
+def subtract first, second
+    first - second
+end
+
+def sum array
+    result = 0
+    array.each do |element|
+        result += element
+    end
+    result
+end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
+# class Timer
+#   attr_reader :seconds
+
+#   def initialize
+#     @seconds = 0
+#   end
+# end
+
 class Timer
-  #write your code here
-end
+  attr_reader :seconds
+  
+  def initialize
+  @seconds=0
+  @time = Time.mktime 2000
+  end
+  
+  def seconds=(number)
+  @time = @time + number
+  end
+  
+  def time_string
+    @time.strftime('%H:%M:%S')
+  end
+  end
